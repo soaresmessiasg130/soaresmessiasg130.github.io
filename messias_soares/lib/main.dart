379c7f3bfcf1ba5361ddada9374ae395d2ed36cb
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2020-11-02 14:40:05 -0300

    Finish Files to Publish

--- messias_soares/lib/main.dart
+++ messias_soares/lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:messias_soares/profile_page.dart';
 
-void main () => runApp(
-  MyPortifolio()
-  );
-  
+void main() => runApp(MyPortifolio());
+
 class MyPortifolio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp (
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData (
-        brightness: Brightness.dark,
-        primaryColor: Colors.black,
-        fontFamily: "GoogleSansRegular"
-      ),
-      home: ProfilePage (),
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.white,
+          fontFamily: "GoogleSansRegular"),
+      home: ProfilePage(),
     );
   }
 }
--- messias_soares/lib/profile_info.dart
+++ messias_soares/lib/profile_info.dart
@@ -2,90 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:messias_soares/responsive_widget.dart';
 
 class ProfileInfo extends StatelessWidget {
-  profileImage (context) => AnimatedContainer (
-    duration: Duration(
-      seconds: 2,
-    ),
-    height: ResponsiveWidget.isSmallScreen(context)
-      ? MediaQuery.of(context).size.height * 0.25
-      : MediaQuery.of(context).size.width * 0.25,
-    
-    width: ResponsiveWidget.isSmallScreen(context)
-      ? MediaQuery.of(context).size.height * 0.25
-      : MediaQuery.of(context).size.width * 0.25,
-    decoration: BoxDecoration (
-      backgroundBlendMode: BlendMode.luminosity,
-      color: Colors.deepPurple,
-      //borderRadius: BorderRadius.circular(40),
-      shape: BoxShape.circle,
-      image: DecorationImage (
-        image: AssetImage(
-          "blackholesemfundo.png"
+  profileImage(context) => AnimatedContainer(
+        duration: Duration(
+          seconds: 2,
         ),
-        alignment: Alignment.center,
-        fit: BoxFit.cover, 
-      ),
-    ),
-  ); 
+        height: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.25
+            : MediaQuery.of(context).size.width * 0.25,
+        width: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.25
+            : MediaQuery.of(context).size.width * 0.25,
+        decoration: BoxDecoration(
+          backgroundBlendMode: BlendMode.luminosity,
+          color: Colors.deepPurple,
+          //borderRadius: BorderRadius.circular(40),
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: AssetImage("blackholesemfundo.png"),
+            alignment: Alignment.center,
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
 
-  final profileData = Column (
+  final profileData = Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
-      Text (
+      Text(
         "Hi there! My name is",
         textScaleFactor: 2,
-        style: TextStyle (
+        style: TextStyle(
           color: Colors.deepPurple,
         ),
       ),
-      Text (
+      Text(
         "Messias\nSoares",
         textScaleFactor: 5,
-        style: TextStyle (
+        style: TextStyle(
           color: Colors.deepPurple,
         ),
       ),
-      SizedBox (
+      SizedBox(
         height: 10,
       ),
-      Text (
-        "Um desenvolvedor BackEnd que ama solucionar problemas.\n"
-        "Buscando implementar ideias focando em qualidade e tempo!\n"
-        "FrontEnd é minha paixão! Me sinto honrado em contribuir\n"
-        "na experiência do usuário: desenvolvendo soluções completas.",
+      Text(
+        // "Um desenvolvedor BackEnd que ama solucionar problemas.\n"
+        // "Buscando implementar ideias focando em qualidade e tempo!\n"
+        // "FrontEnd é minha paixão! Me sinto honrado em contribuir\n"
+        // "na experiência do usuário: desenvolvendo soluções completas.",
+        "An small text",
         softWrap: true,
         textScaleFactor: 1.5,
-        style: TextStyle (
-          color: Colors.white70,
-          fontWeight: FontWeight.bold
-        ),
+        style: TextStyle(color: Colors.white70, fontWeight: FontWeight.bold),
       ),
-      SizedBox (
+      SizedBox(
         height: 20,
       ),
-      Row (
+      Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          RaisedButton (
+          RaisedButton(
             shape: StadiumBorder(),
-            child: Text (
-              "Resume"
-            ),
+            child: Text("Resume"),
             color: Colors.deepPurple,
             onPressed: () {},
             padding: EdgeInsets.all(20),
           ),
-          SizedBox (
+          SizedBox(
             width: 20,
           ),
-          OutlineButton (
-            borderSide: BorderSide (
+          OutlineButton(
+            borderSide: BorderSide(
               color: Colors.deepPurpleAccent,
             ),
             shape: StadiumBorder(),
-            child: Text (
-              "Say Hi!"
-            ),
+            child: Text("Say Hi!"),
             color: Colors.deepPurple,
             onPressed: () {},
             padding: EdgeInsets.all(10),
@@ -97,21 +88,18 @@ class ProfileInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ResponsiveWidget (
-      largeScreen: Row (
+    return ResponsiveWidget(
+      largeScreen: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget> [
-          profileImage(context),
-          profileData
-        ],
+        children: <Widget>[profileImage(context), profileData],
       ),
-      smallScreen: Column (
+      smallScreen: Column(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget> [
+        children: <Widget>[
           profileImage(context),
-          SizedBox (
+          SizedBox(
             height: MediaQuery.of(context).size.height * 0.1,
           ),
           profileData
